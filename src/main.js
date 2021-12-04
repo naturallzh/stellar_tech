045ca1c08e1515d83a714eb7f84992eda7502a82
naturallzh <naturallzh@163.com>
2021-12-04 11:02:10 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-import './plugins/iview.js'
+// import axios from 'axios'
+// import VueAxios from 'vue-axios'
+// import './plugins/iview.js'
 import './assets/styles/init.scss'
 import './assets/styles/public.scss'
-import './assets/styles/iviewOverwrite.scss'
+// import './assets/styles/iviewOverwrite.scss'
 import './assets/styles/variables.scss'
 
 Vue.config.productionTip = false
 
-Vue.use(VueAxios, axios)
+// Vue.use(VueAxios, axios)
 
 new Vue({
   router,
